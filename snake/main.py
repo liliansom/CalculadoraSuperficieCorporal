a6f09051c4198fe711f83002a95d58126e91838c
Lilian Matsumoto <liliansmatsumoto@gmail.com>
2023-02-12 17:07:16 -0300

    Jogo Cobrinha

---
+++ snake/main.py
@@ -0,0 +1,6 @@
+from pygame import font
+
+font = pygame.font.init()
+font_1 = pygame.font.SysFont('Arial', 40, True, True)
+message = 'Pontuação> {}.format(pontos)'
+text = Font.render(font_1, message, False, color=(250, 250, 250))
